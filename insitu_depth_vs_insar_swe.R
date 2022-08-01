11702206801e89cfafebc90965d5b14110a84f39
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2022-08-01 16:42:59 -0700

    plot looks good

--- insitu_depth_vs_insar_swe.R
+++ insitu_depth_vs_insar_swe.R
@@ -73,8 +73,18 @@ sensor_csv_v2 <-cbind(sensor_csv, feb12_19_dswe$insar_feb12_19_dswe, feb19_26_ds
 names(sensor_csv_v2)[7:10] <-c("insar_feb12_19_dswe","insar_feb19_26_dswe","insar_feb12_26_dswe","insar_feb12_26_cm_dswe")
 sensor_csv_v2
 
-#### testing this
-#### convert depth to SWE
+###################
+#### convert depth to SWE for noah's depth sensors
+##################
+
+swe_df <-sensor_csv_v2[1:7,] # chop of hidden valley, wacky data
+swe_df
+
+# new snow density, taking from interval board measurements 
+new_snow_density <- .24
+
+# for the second pair 
+swe_df$feb12_26_dswe <-swe_df$feb12_26*.24
 
 # read in pit data
 pit_info <-read.csv("/Users/jacktarricone/ch1_jemez_data/pit_data/perm_pits.csv")
@@ -84,47 +94,46 @@ pit_info
 # this value will be used for the first pair because there was no new snow
 bulk_density <-mean(pit_info$mean_density[6:7])/1000
 
-# calc SWE change
-sensor_csv_v2$feb12_19_dswe <-sensor_csv_v2$feb12_19*bulk_density
 
-# for the second pair 
-sensor_csv_v2$feb19_26_dswe <-sensor_csv_v2$feb19_26*.15
-sensor_csv_v2$feb12_26_dswe <-sensor_csv_v2$feb12_26*.15
-sensor_csv_v2
+# calc SWE change
+swe_df$feb12_19_dswe <-swe_df$feb12_19*bulk_density
+swe_df$feb19_26_dswe <-swe_df$feb19_26*bulk_density
+swe_df$feb12_26_dswe <-swe_df$feb12_26*bulk_density
+swe_df
 
 #write.csv(sensor_csv_v2, "/Users/jacktarricone/ch1_jemez_data/climate_station_data/noah/insitu_insar_swe_change.csv")
 
 #### format df for plotting and anlysis
 
 # repeat dates so they can be grouped by
-first <-rep(names(sensor_csv_v2[4]), length = nrow(sensor_csv)) 
-second <-rep(names(sensor_csv_v2[5]), length = nrow(sensor_csv)) 
-third <-rep(names(sensor_csv_v2[6]), length = nrow(sensor_csv))
-fourth <-rep("feb12_26_cm",8)
+first <-rep(names(swe_df[4]), length = nrow(swe_df)) 
+second <-rep(names(swe_df[5]), length = nrow(swe_df)) 
+third <-rep(names(swe_df[6]), length = nrow(swe_df))
+fourth <-rep("feb12_26_cm", length = nrow(swe_df))
 date <-c(first, second, third, fourth) # make vector
 
 # repeat meta data
-meta <-rbind(sensor_csv_v2[1:3],sensor_csv_v2[1:3],sensor_csv_v2[1:3],sensor_csv_v2[1:3])
+meta <-rbind(swe_df[1:3],swe_df[1:3],swe_df[1:3],swe_df[1:3])
 
 # bind date vector
 add_date <-cbind(meta, date)
 add_date
 
 # make swe data column in proper order
-insar_dswe <-c(sensor_csv_v2$insar_feb12_19_dswe,
-               sensor_csv_v2$insar_feb19_26_dswe,
-               sensor_csv_v2$insar_feb12_26_dswe,
-               sensor_csv_v2$insar_feb12_26_cm_dswe)
+insar_dswe <-c(swe_df$insar_feb12_19_dswe,
+               swe_df$insar_feb19_26_dswe,
+               swe_df$insar_feb12_26_dswe,
+               swe_df$insar_feb12_26_cm_dswe)
 
 # make insitu column
-insitu_dswe <-c(sensor_csv_v2$feb12_19_dswe,
-                sensor_csv_v2$feb19_26_dswe,
-                sensor_csv_v2$feb12_26_dswe,
-                sensor_csv_v2$feb12_26_dswe)
+insitu_dswe <-c(swe_df$feb12_19_dswe,
+                swe_df$feb19_26_dswe,
+                swe_df$feb12_26_dswe,
+                swe_df$feb12_26_dswe)
 
 # bind together
 plotting_df <-cbind(add_date,insar_dswe,insitu_dswe)
-head(plotting_df)
+plotting_df
 # write.csv(plotting_df, "/Users/jacktarricone/ch1_jemez_data/climate_station_data/noah/insitu_insar_swe_change_plotting_df.csv")
 
 
@@ -144,166 +153,39 @@ lm_eqn <- function(df){
   eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2,
                    list(a = format(unname(coef(m)[1]), digits = 2),
                         b = format(unname(coef(m)[2]), digits = 2),
-                        r2 = format(summary(m)$r.squared, digits = 3)))
+                        r2 = format(summary(m)$r.squared, digits = 2)))
   as.character(as.expression(eq));
 }
 
-ggplot(plotting_df, aes(insitu_dswe, insar_dswe)) +
+# plot
+p <-ggplot(plotting_df, aes(insitu_dswe, insar_dswe)) +
   geom_smooth(method = "lm", se = FALSE)+
   geom_point(aes(color = date)) +
   geom_vline(xintercept = 0, linetype=1, col = "black", alpha = 1) +
   geom_hline(yintercept = 0, linetype=1, col = "black", alpha = 1) +
-  geom_abline(intercept = 0, slope = 1, linetype = 2) +
+  #geom_abline(intercept = 0, slope = 1, linetype = 2) +
   scale_y_continuous(limits = c(-6,6),breaks = c(seq(-6,6,2))) +
   scale_x_continuous(limits = c(-6,6),breaks = c(seq(-6,6,2))) +
   ylab(Delta~"SWE InSAR [cm]") + xlab(Delta~"SWE In situ [cm]") +
   scale_color_manual(name = "Date Range",
                      values = my_colors,
-                     labels = c('Feb 12-19', 'Feb 19-26', 
+                     breaks = c('feb12_19', 'feb19_26',
+                                'feb12_26', 'feb12_26_cm'),
+                     labels = c('Feb 12-19', 'Feb 19-26',
                                 'Feb 12-26', 'Feb 12-26 CM'))+
+  scale_fill_discrete(breaks=c('B', 'C', 'A'))+
   theme(legend.position = c(.85,.25))
 
-p1 <- p + geom_text(x = -4, y = 6, label = lm_eqn(lm_df), parse = TRUE)
+p1 <- p + geom_text(x = -3.28, y = 5, label = lm_eqn(lm_df), parse = TRUE)
 print(p1)
 
 
-## plot old way to checking purposes
-ggplot(sensor_csv_v2) +
-  geom_vline(xintercept = 0, linetype=1, col = "black", alpha = 1) +
-  geom_hline(yintercept = 0, linetype=1, col = "black", alpha = 1) +
-  geom_abline(intercept = 0, slope = 1, linetype = 2) +
-  geom_point(aes(x = feb12_19_dswe, y = insar_feb12_19_dswe, col = "pair1")) +
-  geom_point(aes(x = feb19_26_dswe, y = insar_feb19_26_dswe, col = "pair2")) +
-  geom_point(aes(x = feb12_26_dswe, y = insar_feb12_26_dswe, col = "pair3")) +
-  geom_point(aes(x = feb12_26_dswe, y = insar_feb12_26_cm_dswe, col = "pair4")) +
-  #geom_smooth() +
-  scale_y_continuous(limits = c(-6,6),breaks = c(seq(-6,6,2))) +
-  scale_x_continuous(limits = c(-6,6),breaks = c(seq(-6,6,2))) +
-  ylab(Delta~"SWE InSAR [cm]") + xlab(Delta~"SWE In situ [cm]") +
-  scale_color_manual(name = "Date",
-                     values = c('pair1' = 'darkgreen', 'pair2' = 'plum', 
-                                'pair3' = 'goldenrod', 'pair4' = 'firebrick'),
-                     labels = c('pair1' = 'Feb 12-19', 'pair2' = 'Feb 19-26', 
-                                'pair3' = 'Feb 12-26', 'pair4' = 'Feb 12-26 CM'))
-
-?geom_smooth
-
-# mean of 9 swe changes around
-mean_pit_dswe <-mean(nine_cell_dswe[1:9,1])
-mean_pit_dswe
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# bring in 2/12-2/26 gpr data
-gpr_feb26_minus_feb12_v1 <-rast("./gpr_swe_bias/feb26_minus_Feb12_bias_corrected1.tif")
-gpr_feb26_minus_feb12 <-gpr_feb26_minus_feb12_v1/10 # convert to cm from mm
-plot(gpr_feb26_minus_feb12)
-hist(gpr_feb26_minus_feb12, breaks = 50)
-
-global(gpr_feb26_minus_feb12,mean,na.rm=T)
-
-
-# resample gpr to same grid as unw, crop ext
-i_swe_cum_crop <-crop(i_swe_cum, ext(gpr_feb26_minus_feb12)) # crop
-gpr_feb26_minus_feb12 # check
-i_swe_cum_crop # check
-
-# test plot
-plot(i_swe_cum_crop)
-plot(gpr_feb26_minus_feb12, add = TRUE, col = "red")
-
-# mask unw data with gpr
-i_swe_cum_crop_mask <-mask(i_swe_cum_crop, gpr_feb26_minus_feb12, maskvalue = NA)
-f26_m_12_mask <-mask(gpr_feb26_minus_feb12, i_swe_cum_crop_mask, maskvalue = NA)
-
-# plot only pixels that have data for both gpr and unw
-plot(i_swe_cum_crop_mask)
-plot(f26_m_12_mask, add = TRUE, col = hcl.colors(12, "Berlin"))
-
-# convert raster to dataframe
-swe_df <-as.data.frame(i_swe_cum_crop_mask, xy = TRUE, cells = TRUE, na.rm = TRUE)
-gpr_df <-as.data.frame(f26_m_12_mask, xy = TRUE, cells = TRUE, na.rm = TRUE)
-head(gpr_df)
-head(swe_df)
-
-# bind the data frames
-cm_plotting_df <-cbind(swe_df, gpr_df$feb26_minus_Feb12_bias_corrected1)
-head(cm_plotting_df)
-colnames(cm_plotting_df)[4] <- "dswe_insar" # rename col 4
-colnames(cm_plotting_df)[5] <- "dswe_gpr" # rename col 5
-head(cm_plotting_df)
-
-# quick hists
-hist(cm_plotting_df$dswe_gpr, breaks = 10)
-hist(cm_plotting_df$dswe_insar, breaks = 10)
-
-####
-# plotting
-####
-
-# scattter
-#theme_set(theme_light(11)) # set theme
-theme_set(theme_bw(14)) # set theme
-ggplot(cm_plotting_df) +
-  geom_vline(xintercept = 0) +
-  geom_hline(yintercept = 0) +
-  ylim(c(-10,10)) + xlim(c(-10,10))+
-  geom_point(aes(y = dswe_insar, x = dswe_gpr), color = "darkred", alpha = .3) +
-  labs(#title = Delta~"SWE GPR vs. InSAR 2/12 - 2/26",
-       x = Delta~"SWE GPR [cm]",
-       y = Delta~"SWE InSAR [cm]")+
-  theme(panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank(),
-        panel.background = element_rect(colour = "black", size=1))
-
-# lm_df <-cm_plotting_df[-c(1:3)]
-# names(lm_df)[1:2] <-c("x","y")
-# 
-# lm_eqn <- function(df){
-#   m <- lm(y ~ x, df);
-#   eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
-#                    list(a = format(unname(coef(m)[1]), digits = 2),
-#                         b = format(unname(coef(m)[2]), digits = 2),
-#                         r2 = format(summary(m)$r.squared, digits = 3)))
-#   as.character(as.expression(eq));
-# }
-# 
-# p1 <- p + geom_text(x = -5, y = 9, label = lm_eqn(lm_df), parse = TRUE)
-# print(p1)
-
-ggsave("/Users/jacktarricone/ch1_jemez_data/plots/dswe_gpr_vs_insar_feb26_12_bias_v5.png",
-       width = 5, 
-       height = 5,
-       units = "in",
-       dpi = 300)
-
-# density scatter
-ggplot(cm_plotting_df, aes(y = dswe_insar, x = dswe_gpr)) +
-  #geom_abline(slope = 1) +
-  xlim(c(-2,2)) + ylim(c(-2,2))+
-  stat_density_2d(aes(fill = ..level..), geom = "polygon", contour_var = "count")+
-  scale_fill_continuous(type = "viridis") +
-  labs(#title = Delta~"SWE GPR vs. InSAR 2/12 - 2/26",
-       x = Delta~"SWE GPR [cm]",
-       y = Delta~"SWE InSAR [cm]")
-
 # save image, doesnt like back slahes in the name bc it's a file path... idk
-ggsave("/Users/jacktarricone/ch1_jemez_data/plots/swe_gpr_vs_insar_feb12_26.png",
+ggsave("/Users/jacktarricone/ch1_jemez_data/plots/insitu_swe_vs_insar_swe_v1.png",
        width = 5, 
        height = 5,
        units = "in",
-       dpi = 300)
+       dpi = 400)
 
 
 
