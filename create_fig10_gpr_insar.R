1166920d700489eb2c4dc7ccf098d603ea9deed7
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2023-01-13 12:04:02 -0800

    idk at this point

---
+++ create_fig10_gpr_insar.R
@@ -0,0 +1,125 @@
+# gpr swe vs isce insar swe
+# august 3, 2022
+
+library(terra)
+library(ggplot2);theme_set(theme_classic(12))
+library(dplyr)
+
+# set custom theme
+theme_classic <- function(base_size = 11, base_family = "",
+                          base_line_size = base_size / 22,
+                          base_rect_size = base_size / 22) {
+  theme_bw(
+    base_size = base_size,
+    base_family = base_family,
+    base_line_size = base_line_size,
+    base_rect_size = base_rect_size
+  ) %+replace%
+    theme(
+      # no background and no grid
+      panel.border     = element_blank(),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      
+      # show axes
+      # axis.line      = element_line(colour = "black", linewidth = rel(1)),
+      
+      # match legend key to panel.background
+      legend.key       = element_blank(),
+      
+      # simple, black and white strips
+      strip.background = element_rect(fill = "white", colour = "black", linewidth = rel(2)),
+      # NB: size is 1 but clipped, it looks like the 0.5 of the axes
+      
+      complete = TRUE
+    )
+}
+
+setwd("/Users/jacktarricone/ch1_jemez/")
+
+#######
+#######
+## read in swe change data
+dswe <-rast("./gpr_rasters_ryan/new_swe_change/dswe_feb12-26_sp.tif") # 
+dswe_cm <-rast("./gpr_rasters_ryan/new_swe_change/dswe_feb12-26_cumulative.tif") # cumulative insar pair
+dswe
+dswe_cm
+
+# bring in 2/12-2/26 gpr data
+gpr_feb26_minus_feb12_v1 <-rast("./gpr_swe_bias/feb26_minus_Feb12_bias_corrected1.tif")
+gpr_feb26_minus_feb12 <-gpr_feb26_minus_feb12_v1/10 # convert to cm from mm
+plot(gpr_feb26_minus_feb12)
+hist(gpr_feb26_minus_feb12, breaks = 50)
+#global(gpr_feb26_minus_feb12,mean,na.rm=T)
+
+# resample gpr to same grid as unw, crop ext
+dswe_crop <-crop(dswe, ext(gpr_feb26_minus_feb12)) # crop
+dswe_cm_crop <-crop(dswe_cm, ext(gpr_feb26_minus_feb12)) # crop
+gpr_feb26_minus_feb12 # check
+dswe_crop # check
+dswe_cm_crop # check
+
+# test plot
+plot(dswe_crop)
+plot(dswe_cm_crop)
+plot(gpr_feb26_minus_feb12, add = TRUE, col = "red")
+
+# mask unw data with gpr
+dswe_crop_mask <-mask(dswe_crop, gpr_feb26_minus_feb12, maskvalue = NA)
+dswe_cm_crop_mask_v1 <-mask(dswe_cm_crop, gpr_feb26_minus_feb12, maskvalue = NA)
+dswe_cm_crop_mask <-mask(dswe_cm_crop_mask_v1, dswe_crop_mask, maskvalue = NA)
+f26_m_12_mask <-mask(gpr_feb26_minus_feb12, dswe_crop_mask, maskvalue = NA)
+
+# plot only pixels that have data for both gpr and unw
+plot(dswe_crop_mask)
+plot(dswe_cm_crop_mask)
+plot(f26_m_12_mask, add = TRUE, col = hcl.colors(12, "Berlin"))
+
+# convert raster to dataframe
+df <-as.data.frame(dswe_crop_mask, xy = TRUE, cells = TRUE, na.rm = TRUE)
+cm_df <-as.data.frame(dswe_cm_crop_mask, xy = TRUE, cells = TRUE, na.rm = TRUE)
+gpr_df <-as.data.frame(f26_m_12_mask, xy = TRUE, cells = TRUE, na.rm = TRUE)
+head(gpr_df)
+head(isce_df)
+head(cm_df)
+
+# bind the data frames
+plotting_df <-cbind(df, cm_df[,4] ,gpr_df[,4])
+head(plotting_df)
+colnames(plotting_df)[4] <- "dswe_insar_isce" # rename col 4
+colnames(plotting_df)[5] <- "dswe_insar_cm" # rename col 5
+colnames(plotting_df)[6] <- "dswe_gpr" # rename col 6
+head(plotting_df)
+
+# quick hists
+hist(plotting_df$dswe_gpr, breaks = 20)
+hist(plotting_df$dswe_insar_isce, breaks = 20)
+hist(plotting_df$dswe_insar_cm, breaks = 20)
+
+####
+# plotting
+####
+ggplot(plotting_df) +
+  geom_abline(intercept = 0, slope = 1, linetype = 2) +
+  ylim(c(-6,6)) + xlim(c(-6,6))+
+  geom_point(aes(y = dswe_insar_isce, x = dswe_gpr, color = "isce"), alpha = .5, size =.8) +
+  geom_point(aes(y = dswe_insar_cm, x = dswe_gpr, color = "cm"), alpha = .5, size = .8) +
+  scale_color_manual(name = "InSAR Pair",
+                     values = c('isce' = 'darkviolet', 'cm' = 'goldenrod'),
+                     labels = c('12-26 Feb.', '12-26 Feb. CM'))+
+  labs(x = Delta~"SWE GPR (cm)",
+       y = Delta~"SWE InSAR (cm)")+
+  theme_classic(12) +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size = 1)) +
+  theme(legend.position = c(.2,.83))
+    
+# save
+ggsave("/Users/jacktarricone/ch1_jemez_data/plots/fig10.pdf",
+       width = 5, 
+       height = 5,
+       units = "in",
+       dpi = 400)
+
+
+
+
---
+++ insitu_ts_plot.R
@@ -0,0 +1,156 @@
+# plotting all in situ data in five panel figure
+# jack tarricone
+# july 28th, 2022
+
+# 6 of noah's snow depth sensors on reondo
+# WRCC station: valle grande
+# pit data
+
+library(dplyr)
+library(readxl)
+library(lubridate)
+library(ggplot2);theme_set(theme_classic(12))
+
+# set custom theme
+theme_classic <- function(base_size = 11, base_family = "",
+                          base_line_size = base_size / 22,
+                          base_rect_size = base_size / 22) {
+  theme_bw(
+    base_size = base_size,
+    base_family = base_family,
+    base_line_size = base_line_size,
+    base_rect_size = base_rect_size
+  ) %+replace%
+    theme(
+      # no background and no grid
+      panel.border     = element_blank(),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      
+      # show axes
+      # axis.line      = element_line(colour = "black", linewidth = rel(1)),
+      
+      # match legend key to panel.background
+      legend.key       = element_blank(),
+      
+      # simple, black and white strips
+      strip.background = element_rect(fill = "white", colour = "black", linewidth = rel(2)),
+      # NB: size is 1 but clipped, it looks like the 0.5 of the axes
+      
+      complete = TRUE
+    )
+}
+
+# read in data from noah
+dat <-read_xlsx("/Users/jacktarricone/ch1_jemez/climate_station_data/noah/wy2020/Data_File/nms_wy2020_level0-1.xlsx")
+dat$date_time <-ymd_hms(dat$date_time, tz = "MST") #format to mst
+
+###### format data for plotting
+## reads in with double column/row header that needs to be fixed
+
+# Level 0 = raw data
+# Level 1 = QA/QC data
+
+# remove first row
+dat <-dat[-1,]
+
+# column names string
+headers <-c("date_time", 
+            "DSTempC_1", "DSDepth_1_raw", "DSDepth_1",
+            "DSTempC_2", "DSDepth_2_raw", "DSDepth_2",
+            "DSTempC_3", "DSDepth_3_raw", "DSDepth_3",
+            "DSTempC_4", "DSDepth_4_raw", "DSDepth_4",
+            "DSTempC_5", "DSDepth_5_raw", "DSDepth_5",
+            "DSTempC_6", "DSDepth_6_raw", "DSDepth_6",
+            "DSTempC_7", "DSDepth_7_raw", "DSDepth_7",
+            "DSTempC_8", "DSDepth_8_raw", "DSDepth_8",
+            "DSTempC_9", "DSDepth_9_raw", "DSDepth_9")
+
+# change names
+names(dat) <-headers
+
+# change to numeric
+dat[, 2:28] <- sapply(dat[, 2:28], as.numeric)
+
+# quick test plot
+ggplot(dat)+
+  geom_line(aes(x = date_time, y = DSDepth_6))+
+  lims(y = c(0, 100))
+
+# filter and plot for 2/12-2/26
+filt <-filter(dat, date_time >= "2020-02-12 00:00:00" & date_time <= "2020-03-07 18:50:00")
+
+######## read in HQ met data
+vg_met_data <-read.csv("/Users/jacktarricone/ch1_jemez/climate_station_data/vg/vg_met_data_v2.csv")
+vg_met_data$vg_snow_depth_cm <-as.numeric(vg_met_data$vg_snow_depth_cm)
+vg_met_data$date_time <-mdy_hm(vg_met_data$date_time, tz = "MST")
+vg_met_data$date <-myd(vg_met_data$date)
+
+# filter down to same date range
+vg_filt <-filter(vg_met_data, date_time >= "2020-02-12 00:00:00" & date_time <= "2020-03-07 18:50:00")
+tail(vg_filt$date_time)
+
+# quick test plot
+ggplot(vg_filt)+
+  geom_line(aes(x = date_time, y = vg_snow_depth_cm))+
+  lims(y = c(0, 100))
+
+######### read in redondo met data
+redondo_met_data <-read.csv("/Users/jacktarricone/ch1_jemez/climate_station_data/redondo/redondo_met_data_v2.csv")
+redondo_met_data$date_time <-ymd_hms(redondo_met_data$date_time, tz = "MST")
+redondo_met_data$date <-ymd(redondo_met_data$date)
+
+# filter down to same date range
+redondo_filt <-filter(redondo_met_data, date_time >= "2020-02-12 00:00:00" & date_time <= "2020-03-07 18:50:00")
+tail(redondo_filt$date_time)
+
+# bind snow depth and date_time cols to df
+insar <-cbind(filt,
+              vg_filt$vg_snow_depth_cm)
+
+# rename new cols
+names(insar)[29] <- "vg_snow_depth_cm"
+
+# test_plot
+ggplot(insar) +
+  geom_point(aes(x = date_time, y = vg_snow_depth_cm))
+
+# define flight dates and times
+flight1 <-as.numeric(insar$date_time[9]) # row for correct date time
+flight2 <-as.numeric(insar$date_time[178])
+flight3 <-as.numeric(insar$date_time[345])
+
+# fine storm start and end
+storm_start <-insar$date_time[240]
+storm_end <-insar$date_time[270]
+
+# plot
+ggplot(insar)+
+  theme_classic(12)+
+  geom_vline(xintercept = flight1, linetype=3, col = "black", alpha = .7) +
+  geom_vline(xintercept = flight2, linetype=3, col = "black", alpha = .7) +
+  geom_vline(xintercept = flight3, linetype=3, col = "black", alpha = .7) +
+  annotate("rect", xmin = storm_start, xmax = storm_end,
+           ymin = -Inf, ymax = Inf, alpha = .2)+
+  geom_line(aes(x = date_time, y = DSDepth_1, col = "1"), size = .5)+
+  geom_line(aes(x = date_time, y = DSDepth_3, col = "3"), size = .5)+
+  geom_line(aes(x = date_time, y = DSDepth_4, col = "4"), size = .5)+
+  geom_line(aes(x = date_time, y = DSDepth_6, col = "6"), size = .5)+
+  geom_line(aes(x = date_time, y = DSDepth_7, col = "7"), size = .5)+
+  geom_line(aes(x = date_time, y = DSDepth_9, col = "9"), size = .5)+
+  geom_line(aes(x = date_time, y = vg_snow_depth_cm, col = "10"), size = 1)+
+  #geom_line(aes(x = date_time, y = hv_snow_depth_cm, col = "12"), size = .5)+
+  scale_y_continuous(limits = c(50,120),breaks = c(seq(50,120,10)))+
+  ylab("Depth (cm)") + xlab("Date") +
+  scale_color_manual(name = "Sensor",
+                     values = c('1' = 'darkgreen', '3' = 'plum', 
+                                '4' = 'goldenrod', '6' = 'firebrick', 
+                                '7' = 'darkorange','9'='aquamarine',
+                                '10' = 'red'),
+                     labels = c('1' = 'DS_1', '3' = 'DS_3', 
+                                '4' = 'DS_4', '6' = 'DS_6', 
+                                '7' = 'DS_7', '9' = 'DS_9',
+                                '10' = 'VG'))+
+  scale_x_datetime(breaks = "4 day", 
+                   date_labels="%b %d", 
+                   limits = ymd_hms(c("2020-02-12 01:00:00", "2020-03-07 19:00:00"), tz = "MST"))
